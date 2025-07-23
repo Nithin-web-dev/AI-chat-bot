062249d9e179d5fe92cd42012c01f96df3866066
Nithin Selva J <abij4540@gmail.com>
2025-07-23 20:48:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src', // ✅ point to the folder with index.html
-  build: {
-    outDir: '../dist',     // ✅ put build output outside src
-    emptyOutDir: true,
-  }
 })
